9b7a0f0875d85fadd909f7ce925b692cfd1b995c
msantam2 <msantam2@gmail.com>
2017-05-05 20:11:07 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+entrance_coord = Coordinate.create!(cartesian_point: "(-4, 0)")
+cathys_office_coord = Coordinate.create!(cartesian_point: "(-2, 2)")
+marketing_dept_coord = Coordinate.create!(cartesian_point: "(-2, 3)")
+
+entrance = Feature.create!(name: "Entrance", coordinate_id: entrance_coord.id)
+cathys_office = Feature.create!(name: "Cathy's Office'", coordinate_id: cathys_office_coord.id)
+marketing_dept = Feature.create!(name: "Marketing Dept.", coordinate_id: marketing_dept_coord.id)
